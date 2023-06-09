d91f6f434f03678156fa96527ee5ed315831928c
Andrew <betelgeuse.as@gmail.com>
2023-06-09 22:37:24 +0300

    Install MUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+
+import { Typography } from '@mui/material'
 
 import Header from './components/Header'
 import Dashboard from './pages/Dashboard'
@@ -10,19 +13,23 @@ import Register from './pages/Register'
 export const App = (): JSX.Element => {
   return (
     <>
-      <Router>
-        <div className='container'>
-          <Header />
+      {/*<Router>*/}
+      {/*  <div className='container'>*/}
+      {/*    <Header />*/}
 
-          <Routes>
-            <Route path='/' element={<Dashboard />} />
+      {/*    <Routes>*/}
+      {/*      <Route path='/' element={<Dashboard />} />*/}
 
-            <Route path='/login' element={<Login />} />
+      {/*      <Route path='/login' element={<Login />} />*/}
 
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </div>
-      </Router>
+      {/*      <Route path='/register' element={<Register />} />*/}
+      {/*    </Routes>*/}
+      {/*  </div>*/}
+      {/*</Router>*/}
+
+      <Typography variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
 
       <ToastContainer />
     </>
