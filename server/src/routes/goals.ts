400cfbf7c1e64928af0a2e049338cd473c9574dc
Andrew <betelgeuse.as@gmail.com>
2023-08-02 12:56:33 +0300

    Fix DB connection

--- server/src/routes/goals.ts
+++ server/src/routes/goals.ts
@@ -1,9 +1,9 @@
-const express = require('express')
+import express from 'express'
 
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goal')
 const { protect } = require('../middleware/auth')
 
-const router = express.Router()
+const router: express.Router = express.Router()
 
 router.route('/').get(protect, getGoals).post(protect, setGoal) // * GET POST /api/goals
 router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal) // * DELETE PUT /api/goals/:id
