1a548c14a1e29a2705b0f1f4913ed007493fd02d
Andrew <betelgeuse.as@gmail.com>
2023-08-05 09:05:42 +0300

    Delete old tests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 import { Typography } from '@mui/material'
 
-import Header from './components/Header'
-import Dashboard from './pages/Dashboard'
-import Login from './pages/Login'
-import Register from './pages/Register'
+// import Header from './components/Header'
+// import Dashboard from './pages/Dashboard'
+// import Login from './pages/Login'
+// import Register from './pages/Register'
 
 export const App = (): JSX.Element => {
   return (
