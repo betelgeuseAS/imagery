6bbbca76d41ad8be8e6316a7381a93c9258a9152
Andrew <betelgeuse.as@gmail.com>
2023-09-25 22:22:16 +0300

    Update register page

---
+++ client/src/pages/RegisterPage.tsx
@@ -0,0 +1,124 @@
+import { FC, useEffect, Fragment } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
+import { object, string, TypeOf } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { toast } from 'react-toastify'
+import { useTranslation } from 'react-i18next'
+
+import { Box, Typography } from '@mui/material'
+import { LoadingButton } from '@mui/lab'
+
+import { RootState } from '../redux/store'
+import i18next from '../i18n/config'
+import { createComponents } from '../mui'
+import { useRegisterUserMutation } from '../redux/api/authApi'
+
+import FormInput from '../components/FormInput'
+import FormInputPassword from '../components/FormInputPassword'
+
+const registerSchema = object({
+  name: string().min(1, i18next.t('validation.full_name_required')).max(100, i18next.t('validation.full_name_less')),
+  email: string().min(1, i18next.t('validation.email_required')).email(i18next.t('validation.email_invalid')),
+  password: string()
+    .min(1, i18next.t('validation.password_required'))
+    .min(8, i18next.t('validation.password_more', { number: 8 }))
+    .max(32, i18next.t('validation.password_less', { number: 32 }))
+    .regex(/\d/, i18next.t('validation.password_contains_letter_number', { letter: 1, number: 1 }))
+    .regex(/[a-zA-Z]/, i18next.t('validation.password_contains_letter_number', { letter: 1, number: 1 })),
+  passwordConfirm: string().min(1, i18next.t('validation.password_confirm'))
+}).refine((data) => data.password === data.passwordConfirm, {
+  path: ['passwordConfirm'],
+  // message: i18next.t('validation.password_not_match'),
+  params: { i18n: 'validation.password_not_match' }
+})
+
+export type RegisterInput = TypeOf<typeof registerSchema>
+
+export const RegisterPage: FC = () => {
+  const themeMode = useSelector((state: RootState) => state.uiState.themeMode)
+
+  const { LinkItem } = createComponents(themeMode)
+
+  const { t } = useTranslation()
+
+  const methods = useForm<RegisterInput>({
+    resolver: zodResolver(registerSchema)
+  })
+
+  const [registerUser, { isLoading, isSuccess, error, isError, data }] = useRegisterUserMutation()
+
+  const navigate = useNavigate()
+
+  const {
+    reset,
+    handleSubmit,
+    formState: { isSubmitSuccessful }
+  } = methods
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success(data?.message)
+      navigate('/verify_email')
+    }
+
+    if (isError) {
+      if (Array.isArray((error as any).data.error)) {
+        ;(error as any).data.error.forEach((el: any) =>
+          toast.error(el.message, {
+            position: 'top-right'
+          })
+        )
+      } else {
+        toast.error((error as any).data.message, {
+          position: 'top-right'
+        })
+      }
+    }
+  }, [isLoading])
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset()
+    }
+  }, [isSubmitSuccessful])
+
+  const onSubmitHandler: SubmitHandler<RegisterInput> = (values) => {
+    registerUser(values)
+  }
+
+  return (
+    <Fragment>
+      <Typography variant="h5" gutterBottom>
+        {t('auth.sign_up')}
+      </Typography>
+      <Typography gutterBottom sx={{ color: 'text.disabled' }}>
+        {t('auth.sigh_up_label')}
+      </Typography>
+
+      <FormProvider {...methods}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmitHandler)}
+          noValidate
+          autoComplete="off"
+          maxWidth="21rem"
+          width="100%">
+          <FormInput name="name" label={t('forms.full_name')} size="small" autoFocus />
+          <FormInput name="email" label={t('forms.email')} type="email" size="small" />
+          <FormInputPassword name="password" label={t('forms.password')} size="small" />
+          <FormInputPassword name="passwordConfirm" label={t('forms.confirm_password')} size="small" />
+
+          <Typography align="center" sx={{ color: 'text.disabled', mb: 2 }}>
+            {t('auth.already_have_account')}? <LinkItem to="/login">{t('auth.log_in_here')}</LinkItem>
+          </Typography>
+
+          <LoadingButton variant="contained" fullWidth disableElevation type="submit" loading={isLoading}>
+            {t('auth.sign_up')}
+          </LoadingButton>
+        </Box>
+      </FormProvider>
+    </Fragment>
+  )
+}
